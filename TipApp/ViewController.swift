ee8a346f6f8e4b8ab2afa4d8061c62a8bf5889de
vickyzheng94 <vickyzheng94@gmail.com>
2020-09-05 23:40:46 -0400

    Initial Commit

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by Vicky Zheng on 9/5/20.
+//  Copyright © 2020 Vicky Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
